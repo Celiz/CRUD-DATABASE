6ceb4efa8d2e914756ac56ac0a277e4d73d60306
Matias Celiz <82237565+Celiz@users.noreply.github.com>
2022-09-12 16:07:06 -0300

    definitivo

--- add2.php
+++ add2.php
@@ -1,5 +1,5 @@
 <?php
-   $mysql=new mysqli("$host","$user","$password","$database");
+   $mysql=new mysqli($host,$user,$password,$database);
 	if ($mysql->connect_error)
 	  die("Problemas con la conexion a la base de datos");
 
