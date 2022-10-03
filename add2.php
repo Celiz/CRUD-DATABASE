0d2da59520e09910420ccfb6d6d3e9acd72a4bb8
Matias Celiz <82237565+Celiz@users.noreply.github.com>
2022-10-03 07:52:14 -0300

    changes!

--- add2.php
+++ add2.php
@@ -1,20 +1,23 @@
 <?php
-   $mysql=new mysqli($host,$user,$password,$database);
+    $mysql=new mysqli("localhost","457327","lunaylaky2017","457327");
 	if ($mysql->connect_error)
 	  die("Problemas con la conexion a la base de datos");
 
-    $foto = $_FILES['foto_vivienda']['name'];
-    $foto_temporal = $_FILES['foto_vivienda']['tmp_name'];
-    $destino = "image/".$foto;
-    move_uploaded_file($foto_temporal,$destino . $foto);
-    copy($foto_temporal,$destino);
+ 
+    $target_dir = "./image/";
+    $target_file = $target_dir . basename($_FILES["foto_vivienda"]["name"]);
+
+   if (move_uploaded_file($_FILES["foto_vivienda"]["tmp_name"], $target_dir.$_FILES['foto_vivienda']['name'])) {
+   $status = 1;
+   }
+    
   
     $mysql->query("insert into viviendas (tipo_vivienda, zona_vivienda, direccion_vivieda, ndormitorios_vivienda, precio_vivienda, tamano_vivienda, extras_vivienda, foto_vivienda, observaciones_vivienda)
-        values ('$_REQUEST[tipo_vivienda]', '$_REQUEST[zona_vivienda]', '$_REQUEST[direccion_vivieda]', '$_REQUEST[ndormitorios_vivienda]', '$_REQUEST[precio_vivienda]', '$_REQUEST[tamano_vivienda]', '$_REQUEST[extras_vivienda]', '$destino', '$_REQUEST[observaciones_vivienda]')") 
+        values ('$_REQUEST[tipo_vivienda]', '$_REQUEST[zona_vivienda]', '$_REQUEST[direccion_vivieda]', '$_REQUEST[ndormitorios_vivienda]', '$_REQUEST[precio_vivienda]', '$_REQUEST[tamano_vivienda]', '$_REQUEST[extras_vivienda]', '$target_file', '$_REQUEST[observaciones_vivienda]')") 
         or
         die($mysql->error);
+        
+     $mysql->close();    
 
-    $mysql->close();
-
-    header('Location:index.php');    
+   header('Location:index.php');     
 ?>  
